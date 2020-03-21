7c6a5e29c8794c3ae4be78b563abcac59648b564
Leonard von Lojewski <37808546+cadast@users.noreply.github.com>
2020-03-21 22:06:32 +0100

    new api key obtaining

--- app/services/DistanceService.php
+++ app/services/DistanceService.php
@@ -11,15 +11,9 @@ use GuzzleHttp\Client;
 class DistanceService
 {
 
-    public function __construct()
-    {
-
-    }
-
     public function dist(string $start, string $end) {
         $client = new Client();
-        $api_key = 'AIzaSyAZpc1apVCngmenB0ChSp-U2l5sFD4LhtI';
-        $res = $client-> get('https://maps.googleapis.com/maps/api/distancematrix/json', ['query' => ['origins' =>  $start, 'destinations' => $end, 'key' => $api_key]]);
+        $res = $client-> get('https://maps.googleapis.com/maps/api/distancematrix/json', ['query' => ['origins' =>  $start, 'destinations' => $end, 'key' => env('GMAPS_KEY')]]);
         $result['json'] = json_decode($res->getBody());
         $result['status'] = $res->getStatusCode();
         $result['header'] = $res->getHeader('content-type');
@@ -28,19 +22,17 @@ class DistanceService
 
     public function reverseGeo(float $lat,float $lng) {
         $client = new Client();
-        $api_key = 'AIzaSyAZpc1apVCngmenB0ChSp-U2l5sFD4LhtI';
         $latlng = $lat . "," . $lng;
-        $res = $client-> get('https://maps.googleapis.com/maps/api/geocode/json', ['query' => ['latlng' =>  $latlng, 'key' => $api_key]]);
+        $res = $client-> get('https://maps.googleapis.com/maps/api/geocode/json', ['query' => ['latlng' =>  $latlng, 'key' => env('GMAPS_KEY')]]);
         $result['json'] = json_decode($res->getBody());
         $result['status'] = $res->getStatusCode();
         $result['header'] = $res->getHeader('content-type');
         return $result;
     }
 
-    public function Address2Geo(string address) {
+    public function address2Geo(string $address) {
         $client = new Client();
-        $api_key = 'AIzaSyAZpc1apVCngmenB0ChSp-U2l5sFD4LhtI';
-        $res = $client-> get('https://maps.googleapis.com/maps/api/geocode/json', ['query' => ['address' =>  $address, 'key' => $api_key]]);
+        $res = $client-> get('https://maps.googleapis.com/maps/api/geocode/json', ['query' => ['address' =>  $address, 'key' => env('GMAPS_KEY')]]);
         $result['json'] = json_decode($res->getBody());
         $result['status'] = $res->getStatusCode();
         $result['header'] = $res->getHeader('content-type');
