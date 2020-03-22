76db22bffec8561965dc84fedd40981a8bffe7c2
Leonard von Lojewski <37808546+cadast@users.noreply.github.com>
2020-03-23 00:09:03 +0100

    Throw exception if GMaps is fucked

--- app/User.php
+++ app/User.php
@@ -93,11 +93,17 @@ class User extends Authenticatable
         return $this->hasOne(Supplier::class);
     }
 
+    /**
+     * @throws \Exception
+     */
     public function getLatLong()
     {
         if ($this->isDirty(['streetName', 'houseNumber', 'city', 'postcode']) || $this->wasRecentlyCreated) {
             $ds = new DistanceService();
             $res = $ds->address2Geo($this->getAddress());
+            if ($res['json']['status'] !== 'OK') {
+                throw new \Exception('Error with GMaps. Response: ' . json_encode($res['json']));
+            }
             $this->latitude = $res['json']['results'][0]['geometry']['location']['lat'];
             $this->longitude = $res['json']['results'][0]['geometry']['location']['lng'];
         }
